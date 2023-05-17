74bdb070f019ac7fd171e2a353731092a6971187
Naveed Ullah <naveed.dev30@gmail.com>
2023-05-17 17:26:31 +0500

    up

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,25 +3,31 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use App\Http\Controllers\ProposalController;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
-    protected function schedule(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
-    }
+  /**
+   * Define the application's command schedule.
+   */
+  protected function schedule(Schedule $schedule): void
+  {
+    $schedule
+      ->call(function () {
+        $proposalController = new ProposalController();
+        $proposalController->getProposals(); // Replace with your actual method name
+      })
+      ->everySeconds(30);
+  }
 
-    /**
-     * Register the commands for the application.
-     */
-    protected function commands(): void
-    {
-        $this->load(__DIR__.'/Commands');
+  /**
+   * Register the commands for the application.
+   */
+  protected function commands(): void
+  {
+    $this->load(__DIR__ . '/Commands');
 
-        require base_path('routes/console.php');
-    }
+    require base_path('routes/console.php');
+  }
 }
