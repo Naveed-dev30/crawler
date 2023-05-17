c4c10a23e341dacb573eda7a3714606e71481032
Naveed Ullah <naveed.dev30@gmail.com>
2023-05-17 15:23:43 +0500

    update

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -8,80 +8,79 @@ use App\Models\Bid;
 
 class BidController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $bids = Bid::all();
-        return view('content.pages.home', ['bids' => $bids]);
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $bids = Bid::all()->sortByDesc('created_at');
+    return view('content.pages.home', ['bids' => $bids]);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreBidRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreBidRequest $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \App\Http\Requests\StoreBidRequest  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(StoreBidRequest $request)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Bid  $bid
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Bid $bid)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\Bid  $bid
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Bid $bid)
+  {
+    return view('content.pages.filter_edit', ['bid' => $bid]);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Bid  $bid
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Bid $bid)
-    {
-        return view('content.pages.filter_edit', ['bid' => $bid]);
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Models\Bid  $bid
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Bid $bid)
+  {
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateBidRequest  $request
-     * @param  \App\Models\Bid  $bid
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateBidRequest $request, Bid $bid)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \App\Http\Requests\UpdateBidRequest  $request
+   * @param  \App\Models\Bid  $bid
+   * @return \Illuminate\Http\Response
+   */
+  public function update(UpdateBidRequest $request, Bid $bid)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Bid  $bid
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Bid $bid)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Bid  $bid
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Bid $bid)
+  {
+    //
+  }
 }
