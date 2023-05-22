4c21c3221f96b7e8490456bf70b248b5767a4868
Naveed Ullah <naveed.dev30@gmail.com>
2023-05-23 00:21:55 +0500

    Update

--- app/Http/Controllers/ProposalController.php
+++ app/Http/Controllers/ProposalController.php
@@ -101,8 +101,7 @@ class ProposalController extends Controller
     $yesterday = $now->subDay()->unix();
     $accessAuthToken = 'uvsN2826QWbr1gVlRWrhaQJf5oX16o';
     $params = [
-      // 'query' => 'flutter firebase android ios dart app mobile app ux ui landing page worpress logo graphics web app',
-      'query' => 'flutter',
+      'query' => 'flutter firebase android ios dart app mobile app',
       'from_time' => $yesterday,
       'limit' => 100,
       'min_price' => $filter->min_fixed_amount,
