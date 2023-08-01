98546ce2aa604906c69558d48f549e593071c095
Naveed <naveed.dev30@gmail.com>
2023-08-01 18:02:37 +0500

    Update

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
 
     'slack' => [
         'notifications' => [
-            'bot_user_oauth_token' => 'xapp-1-A05KA0855E3-5656451563783-6041868641f1a2f59fef97e1803bdb05365845dcb6eaefbcb525bc3dc8da325f',
+            'bot_user_oauth_token' => '***REMOVED-SLACK-TOKEN***',
             'channel' => 'stack',
         ],
     ]
