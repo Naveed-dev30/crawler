387171e9cf93f0ff52ce1c23e527df9de441e627
Naveed <naveed.dev30@gmail.com>
2024-04-25 00:37:48 +0500

    Update

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -83,7 +83,6 @@ class FilterController extends Controller
     public function update(Request $request)
     {
         try {
-            return redirect('/');
             $countries = $request->formValidationCountries;
             $currencies = $request->formValidationCurrencies;
             $prompt = $request->formValidationPrompt;
