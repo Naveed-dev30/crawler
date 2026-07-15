5ad903e21bc494eca8c71fa096ffc2d3dc1ee0e4
Irfan Ali <irfan.196.id@gmail.com>
2026-07-15 16:58:03 +0500

    feat: redesign dashboard cards, add sticky filter bar, and implement tooltips for bid review statuses

--- resources/views/_partials/bid-row.blade.php
+++ resources/views/_partials/bid-row.blade.php
@@ -5,6 +5,9 @@
     $checkIcon = $bid->check === 'Correct'
         ? 'fa fa-check text-success'
         : ($bid->check === 'Incorrect' ? 'fa fa-close text-danger' : 'fa fa-warning text-warning');
+    $checkLabel = $bid->check === 'Correct'
+        ? 'Marked Correct'
+        : ($bid->check === 'Incorrect' ? 'Marked Incorrect' : 'Unreviewed');
 @endphp
 <tr>
     <td>{{ $bid->proposal->project_id }}</td>
@@ -19,9 +22,12 @@
         </div>
     </td>
     <td>
-        <button type="button" class="btn btn-sm btn-outline-primary bid-view-btn" data-bid-id="{{ $bid->id }}">
-            <i class="{{ $bid->is_seen ? 'fa fa-eye text-success' : 'fa fa-eye' }} me-1"></i> View
-        </button>
-        <i class="{{ $checkIcon }} ms-1" data-check-dot="{{ $bid->id }}"></i>
+        <div class="d-flex align-items-center gap-2">
+            <button type="button" class="btn btn-sm btn-label-primary bid-view-btn" data-bid-id="{{ $bid->id }}">
+                <i class="{{ $bid->is_seen ? 'fa fa-eye text-success' : 'fa fa-eye' }} me-1"></i> View
+            </button>
+            <i class="{{ $checkIcon }}" data-check-dot="{{ $bid->id }}" style="cursor: help;"
+               data-bs-toggle="tooltip" data-bs-custom-class="tooltip-light" title="{{ $checkLabel }}"></i>
+        </div>
     </td>
 </tr>
